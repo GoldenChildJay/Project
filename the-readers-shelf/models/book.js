78c20e14fe50c40cb6ba062a0eb7218c52013d6a
GoldenChildJay <jhoonie4@gmail.com>
2023-07-23 15:56:28 +0100

    prohject

---
+++ the-readers-shelf/models/book.js
@@ -0,0 +1,30 @@
+// models/book.js
+
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  genre: {
+    type: String,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    required: true,
+  },
+  summary: {
+    type: String,
+    required: true,
+  },
+});
+
+const Book = mongoose.model('Book', bookSchema);
+
+module.exports = Book;
